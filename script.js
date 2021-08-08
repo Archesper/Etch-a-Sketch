513b743e494b6e7625af748a8d99baf975797f4b
Archesper <pk4nde@gmail.com>
2021-08-08 21:21:12 +0100

    Add clear button and grid resizing

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const grid = document.querySelector("#grid")
+fillGrid(16);
+
+const clearButton = document.getElementById("clearButton");
+clearButton.addEventListener('click', newGrid)
+
 function colorSwitch(e) {
     const red = Math.floor(Math.random()*255);
     const green = Math.floor(Math.random()*255);
@@ -5,13 +11,23 @@ function colorSwitch(e) {
     e.target.style.backgroundColor = `rgb(${red}, ${blue}, ${green})`;
 }
 
-
-const grid = document.querySelector("#grid")
-for (i = 0; i <= 16*16; i++) {
-    grid.appendChild(document.createElement('div'));
+function newGrid() {
+    let newGridSize = window.prompt("Enter new grid size: (Max 100)");
+    console.log(newGridSize);
+    while (isNaN(newGridSize) || parseInt(newGridSize) > 100) {
+        newGridSize = window.prompt("Please input a number lower than or equal to:");
+    }
+    if (newGridSize === null) {return;}
+    fillGrid(parseInt(newGridSize));
 }
-const gridCells = document.querySelectorAll("#grid>div");
-gridCells.forEach(cell => cell.addEventListener('mouseover', colorSwitch));
 
-
-
+function fillGrid(size) {
+    grid.innerHTML = '';
+    grid.style.setProperty('grid-template-rows', `repeat(${size}, 1fr)`);
+    grid.style.setProperty('grid-template-columns', `repeat(${size}, 1fr)`);
+    for (i = 0; i <= size*size; i++) {
+        grid.appendChild(document.createElement('div'));
+    }
+    const gridCells = document.querySelectorAll("#grid>div");
+    gridCells.forEach(cell => cell.addEventListener('mouseover', colorSwitch));
+}
